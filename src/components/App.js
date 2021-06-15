27f4c8a364ccdbab111322d895ac5d82b409c50e
Ebraj <gurungebraj22@gmail.com>
2021-06-15 14:41:49 +0545

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.scss";
+import Main from "./Main";
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <Main />
     </div>
   );
 };
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <p>Main Page</p>
+    </div>
+  );
+};
+
+export default Main;
